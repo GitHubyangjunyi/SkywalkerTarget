5e96d9748bcc727e178da3fa9dbedfad4eac7f66
杨俊艺 <1209032479@qq.com>
2022-02-18 18:14:05 +0800

    更新子模块

--- SkywalkerTarget/ViewController.swift
+++ SkywalkerTarget/ViewController.swift
@@ -6,18 +6,73 @@
 //
 
 import UIKit
+import Skywalker
 
 class ViewController: UIViewController {
 
+    private let timeFormatter = DateFormatter.init().then { make in
+        make.dateFormat = "yyyy-MM-dd' 'HH:mm:ss.SSSS"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        
+        var time = Date.init()
+        var timeStr = timeFormatter.string(from: time)
+        var timeAndURLStamp = timeStr.appending("cc")
+        
+        SwiftSignposter.shared.signpostNetworkGetBeginWith(url: "cc", timeAndURLStamp: timeAndURLStamp as NSString)
+        for _ in 0...10000 {
+            for _ in 0...1000 {
+                print("c")
+            }
+        }
+        SwiftSignposter.shared.signpostNetworkGetEndWith(url: "cc", timeAndURLStamp: timeAndURLStamp as NSString)
+        
+        
+        time = Date.init()
+        timeStr = timeFormatter.string(from: time)
+        timeAndURLStamp = timeStr.appending("aa")
+        
+        SwiftSignposter.shared.signpostNetworkPostBeginWith(url: "pp", timeAndURLStamp: timeAndURLStamp as NSString)
+        for _ in 0...10000 {
+            for _ in 0...1000 {
+                print("c")
+            }
+        }
+        SwiftSignposter.shared.signpostNetworkPostEndWith(url: "pp", timeAndURLStamp: timeAndURLStamp as NSString)
+        
+        SwiftSignposter.shared.signpostFlashEventWith(id: 888)
+        
+        
+        time = Date.init()
+        timeStr = timeFormatter.string(from: time)
+        timeAndURLStamp = timeStr.appending("aa")
+        
+        SwiftSignposter.shared.signpostNetworkGetBeginWith(url: "aa", timeAndURLStamp: timeAndURLStamp as NSString)
+        for _ in 0...10000 {
+            for _ in 0...1000 {
+                print("c")
+            }
+        }
+        SwiftSignposter.shared.signpostNetworkGetEndWith(url: "aa", timeAndURLStamp: timeAndURLStamp as NSString)
+        
+        SwiftSignposter.shared.signpostFlashEventWith(id: 666)
         
         
         
+        SwiftSignposter.shared.signpostBeginWith(id: 123)
+        for _ in 0...10000 {
+            for _ in 0...1000 {
+                print("c")
+            }
+        }
+        SwiftSignposter.shared.signpostEndWith(id: 123)
         
         
-        
-        
+        SwiftSignposter.shared.signpostFlashEventWith(id: 777)
         
         
 //        let vc = OCViewController.init()
